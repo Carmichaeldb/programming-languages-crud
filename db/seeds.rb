fe8ea0a196891cc2097513cf9ea8a5c38d47966c
David Carmichael <carmichael.db88@gmail.com>
2024-03-28 21:20:48 +0000

    Add seed data for programming languges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ProgrammingLanguage.create([
+  { title: 'Ruby', description: 'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.', link: 'https://www.ruby-lang.org' },
+  { title: 'Python', description: 'Python is a programming language that lets you work quickly and integrate systems more effectively.', link: 'https://www.python.org' },
+  { title: 'JavaScript', description: 'JavaScript is a programming language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+])
